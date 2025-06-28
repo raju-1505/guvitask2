5bf8cdf3d9068e0475f47be0541a58f0cfa26328
raju-1505 <rajiraju0078@gmail.com>
2025-06-28 16:18:56 +0530

    q1 Person.java

--- Person.java
+++ Person.java
@@ -24,3 +24,19 @@ package Qone;//1.1 create class person with properties
 }
 
 
+
+public class PersonMain {
+    public static void main(String[] args) {
+
+        Person p1 = new Person("Dinesh");
+
+
+        Person p2 = new Person("Raju", 25);
+
+        System.out.println("Qone.Person 1 Details:");
+        p1.display();
+
+        System.out.println("\nQone.Person 2 Details:");
+        p2.display();
+    }
+}
